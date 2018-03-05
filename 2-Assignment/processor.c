4dee5defc14f34620b829d931167e9c1b4866b3e
denelor <joshlane127@gmail.com>
2018-03-04 21:26:55 -0800

    assignment 2 v1

--- 2-Assignment/processor.c
+++ 2-Assignment/processor.c
@@ -2,7 +2,15 @@
 Joshua Lane
 UserID: JAL987
 CS-GY 6233 Intro to Operating Systems
-Assignment #2
+Assignment #2, processor.c
+
+Summary:  	This program operates by connecting to shared memory already created
+			by receiver.c.  It watched the first character in that shared memory
+			to determine what it should do next.  This first character acts as 
+			a control signal, with all necessary data following it.  
+			Upon receiving a non-wait signal, this program will either clean up 
+			and then quit or it will process the secret from shared memory into
+			the file secrets.out.  It will the return to a wait condition. 
 */
 
 #include <stdio.h>
@@ -10,8 +18,124 @@ Assignment #2
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
-#define BADPIPE "|"
-#define BADREDIRECTR ">"
-#define BADREDIRECTL "<"
 #define QUIT "QUIT"
+
+//The following characters will determine the behavior of the two processes
+//One of them will always be present in the first space of the buffer
+
+//# tells processor.c to wait for input and to trigger receiver.c to continue
+//! tells processor.c process the string that follows.  
+//A second ! also tells processor.c where the string ends
+//@ tells processor.c that receiver.c has received a request to quit
+#define WAIT '#'
+#define STRING '!'
+#define QUITSIG '@'
+
+#define TEXT_BUFFER_KEY 5555
+#define IPC_BUFFER_SIZE 1024 
+
+//The main menu block that drives user interaction
+//Alerts the user to how many secrets have been processed. 
+void printMenu(int secretNumber){
+	printf("\n\n########  SHARED MEMORY PROCESSOR START  ###########\n");
+	printf("%d secrets shared.\n",secretNumber);
+}
+
+void main(){
+
+	char *shm_text_ptr, *temp; 
+	char secretPhrase[IPC_BUFFER_SIZE];
+	bool quit = false;
+	int shm_text, secretLength, i;
+	int secretNumber=0;
+	FILE *secrets;
+
+	//Connect to the shared memory communications channel already created by receiver.c
+	//the shm_text channel will serve to pass text back and forth
+	shm_text = shmget(TEXT_BUFFER_KEY,IPC_BUFFER_SIZE, IPC_CREAT | 0666);
+
+	if (shm_text < 0){
+		printf("ERROR: shm_text shmget failure\n");
+	}
+	shm_text_ptr = shmat(shm_text, NULL, 0);
+
+	if (shm_text_ptr == "-1"){
+		printf("ERROR: shm_text shmat failure\n");
+	}
+
+	//open secrets.out for writing
+	secrets = fopen("secrets.out","w");
+
+	if (secrets == NULL){
+		printf("ERROR:  secrets.out failed to open.");
+		printf("ERROR:  exiting...");
+	}
+
+	while(quit == false){
+		//reset the length of our current secret
+		secretLength = 0;
+
+		//print the menu to terminal
+		printMenu(secretNumber);	
+
+		//alert user to wait condition and then wait for a signal from receiver
+		printf("Waiting for a secret to be passed.\n");
+		while (shm_text_ptr[0] == WAIT){
+			sleep(1);
+		}
+
+		//Once the wait signal has changed, check if the new signal is a quit
+		if ((shm_text_ptr[0]) == QUITSIG){
+
+			printf("Processor.c has received a quit signal from the receiver.\n");
+			printf("Cleaning up and quiting now.\n");
+			
+			//close our secrets file
+			fclose(secrets);
+
+			//kill our infinite loop
+			quit = true;
+
+		}
+		else {
+			//if the signal is not a quit, it must be a secret to process
+			printf("Processing secret information received from the receiver.\n");
+
+			//temporary pointer so we don't mess with shm_text_ptr
+			temp = shm_text_ptr;
+
+			//use temp to process the secret from shared memory
+			//calculate the length of the secret while also 
+			//copying it to a temporary variable
+			while(temp[secretLength + 1] != STRING){
+				secretPhrase[secretLength] = temp[secretLength + 1];
+				secretLength++;
+			}
+
+			//print the length of the current secret to file
+			fprintf(secrets, "%d|",secretLength);
+
+			//print the secret one character at a time to the file
+			for(i=0; i<secretLength;i++){
+				fprintf(secrets, "%c",secretPhrase[i]);
+			}
+
+			//move the file to a new line to be ready for the next secret
+			fprintf(secrets, "\n");
+
+			//set the first character of the communications buffer to the wait signal
+			//This tells the receiver that the processor is done with it's function and 
+			//is returning to a wait status.  Receiver can exit wait and continue.
+			shm_text_ptr[0] = WAIT;
+
+			//increment the number of secrets passed
+			secretNumber++;
+		}
+
+	}
+	shmdt(shm_text_ptr);
+	return;
+}
--- 2-Assignment/receiver.c
+++ 2-Assignment/receiver.c
@@ -2,7 +2,22 @@
 Joshua Lane
 UserID: JAL987
 CS-GY 6233 Intro to Operating Systems
-Assignment #2
+Assignment #2, receiver.c
+
+Summary:  	This program operates by creating shared memory for communications
+			between it and processor.c.  The first character in that shared memory
+			determines how both programs behave.  This first character acts as 
+			a control signal, with all necessary data following it in all cases.  
+			The receiver takes text input from the user, scans it for the key word
+			anywhere in the input and then reacts accordingly.  If the key word is
+			found, the program places the user input into the buffer and signals 
+			that the processor should execute it's functions.  The receiver then
+			enters a wait status until it sees a signal in the shared memory that
+			the processor is done and has returned to a wait status.  At that 
+			point the receiver prompts for more user input.  This continues until 
+			a QUIT is received, upon which the receiver signals the process to 
+			quit and then quits itself.  
+
 */
 
 #include <stdio.h>
@@ -16,27 +31,58 @@ Assignment #2
 
 #define SECRET_KEY "COOL"
 #define QUIT "QUIT"
+
 #define IN_BUFFER_SIZE 512
-#define DONE_WRITE "%" //The character that the receiver will write to l
-#define DONE_READ "#"
+
+//The following characters will determine the behavior of the two processes
+//One of them will always be present in the first space of the buffer
+
+//# tells processor.c to wait for input and to trigger receiver.c to continue
+//! tells processor.c process the string that follows.  
+//A second ! also tells processor.c where the string ends
+//@ tells processor.c that receiver.c has received a request to quit
+#define WAIT '#'
+#define STRING '!'
+#define QUITSIG '@'
 
 
+#define TEXT_BUFFER_KEY 5555
+#define IPC_BUFFER_SIZE 1024 //large than the input buffer to ensure that input doesn't overflow the text buffer
 
 //The main menu block that drives user interaction
 void printMenu(){
 	printf("\n\n########  SHARED MEMORY PROGRAM START  ###########\n\n");
 	printf("Please enter a single phrase no more than %d bytes.\n", IN_BUFFER_SIZE);
 	printf("Make sure your phrase includes \"COOL\" if you want it to be shared.\n");
+	printf("The '!', '@', and '#' characters are reserved and can not be used in secrets.\n");
 	printf("Please type QUIT to end.\n");
 	printf("Secret Phrase: ");
-
 }
 
 void main(){
 
 	char *input = malloc(sizeof(char)*IN_BUFFER_SIZE);
-	char *temp;
+	char *temp, *shm_text_ptr;
 	bool quit = false;
+	int shm_text;
+
+
+	//Set up the shared memory communications channel
+	//the shm_text channel will serve to pass text back and forth
+	shm_text = shmget(TEXT_BUFFER_KEY,IPC_BUFFER_SIZE, 0666);
+
+	if (shm_text < 0){
+		//handle hte error
+	}
+
+	shm_text_ptr = shmat(shm_text, NULL, 0);
+
+	if (shm_text_ptr == "-1"){
+		//handle error
+	}
+
+	//Set the signal buffer so processor waits when it initially runs.  
+	shm_text_ptr[0] = WAIT;
 
 	while(quit == false){
 		printMenu();	
@@ -45,25 +91,47 @@ void main(){
 		if (strncmp(input,QUIT,4) == 0){
 
 			//logic flow output used to test while building program
-			printf("YOU JUST ASKED TO QUIT\n");
+			printf("Quitting both programs now.\n");
 			
+			//set the first character of the channel to the quit signal
+			shm_text_ptr[0] = QUITSIG;
 
+			//cancel the infinite loop
 			quit = true;
 		}
+		else if (strstr(input,SECRET_KEY)!= NULL){
+			
+			//Skip loading the signal character to the first slot of the buffer.  
+			//This avoid the processor executing before the receiver has finished. 
+			//Receiver will put the signal character in after it has prepared and loaded
+			//all necessary data into the shared memory buffer.  
+			temp = shm_text_ptr;
+			temp++;
 
-		if (strstr(input,SECRET_KEY)!= NULL){
-			//logic flow output used to test while building program
-			temp = input;
-			temp += strlen(input);
-			*temp = DONE_WRITE;
-			printf("YOU ENTERED THE SECRET KEY AS PART OF YOUR PHRASE %s \n", input);
+			//Move the input, temporarily including the newline character, to the communication buffer
+			memcpy(temp,input, strlen(input));
 
+			//Add the string termination character in place of the newline character
+			//The minus one facilitates the overwrite of the newline character
+			temp += strlen(input) - 1;
+			*temp = STRING;
+
+			//The last act is to put the signal character into the buffer
+			shm_text_ptr[0] = STRING;
+			printf("Please wait while the processor handles the secret data.\n");
 		}
 		else{
-			//logic flow output used to test while building program
-			printf("YOU DIDNOT ENTER THE SECRET KEY AS PART OF YOUR PHRASE\n");			
+			
+			//This is the feedback for the case where the user did not enter the secret phrase
+			printf("This phrase did not containt eh secret key and will not be shared. \n");			
 		}
-	}
 
+		while (shm_text_ptr[0] == STRING){
+			sleep(1);
+		}
+		
+	}
+	shmdt(shm_text_ptr);
+	shmctl(shm_text,IPC_RMID,NULL);
 	return;
 }
